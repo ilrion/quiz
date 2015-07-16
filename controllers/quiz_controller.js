ded17290f5d55fc5bf92c0e8c0ed5cc7f0d59c4b
Roberto <ro.jimenez.muncha@gmail.com>
2015-07-16 08:37:55 +0200

    Editar pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -17,7 +17,7 @@ exports.load = function(req, res, next, quizId) {
 
 exports.index = function(req, res) {
   if (req.query.search) {
-    var search_like = '%' + req.query.search.replace(' ','%') + '%';
+    var search_like = '%' + req.query.search.replace('/[ ]+/g','%') + '%';
 
     console.log('Dentro de index con search_like: ' +  search_like);
 
@@ -79,5 +79,32 @@ exports.create = function(req, res) {
       });   // res.redirect: Redirección HTTP a lista de preguntas
     }
   });
+};
+
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+  var quiz = req.quiz;  // req.quiz: autoload de instancia de quiz
+
+  res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+  req.quiz.pregunta  = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+  req.quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+      } else {
+        req.quiz     // save: guarda campos pregunta y respuesta en DB
+        .save( {fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes');});
+      }     // Redirección HTTP a lista de preguntas (URL relativo)
+    }
+  );
 };
 
