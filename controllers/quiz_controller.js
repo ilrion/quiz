a58a8e79604ee652cc56072cc3ddca2c7a519ead
Roberto <ro.jimenez.muncha@gmail.com>
2015-07-23 15:01:39 +0200

    Crear Comentario

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -3,8 +3,15 @@ var models = require('../models/models.js');
 // Autoload - PARAM Se carga lo primero si llega el parametro quizId
 
 exports.load = function(req, res, next, quizId) {
-  models.Quiz.findById(quizId).then(
-    function(quiz) {
+  //models.Quiz.findById({
+  models.Quiz.find({
+    where: {
+      id: Number(quizId)
+    },
+    include: [{
+      model: models.Comment
+    }]
+  }).then(function(quiz) {
       if (quiz) {
         req.quiz=quiz;
         next();
