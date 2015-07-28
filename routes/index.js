87e6037d6192e37a44b194c64e8c6499b892ad4e
Roberto <ro.jimenez.muncha@gmail.com>
2015-07-28 10:28:04 +0200

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 // Autoload cuando en una petición llega el parámetro quizId
 router.param('quizId', quizController.load);
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -37,5 +38,7 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired,
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
+             sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
